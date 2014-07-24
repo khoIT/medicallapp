6a7499463cf97283214e5b3f78f7aae5def00989
Khoi Tran <emails.khoi@gmail.com>
2014-07-23 18:24:34 -0700

    Create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Medicallapp::Application.config.secret_key_base = '9bc90dd3461ded52b0c6d5bc80db3ff5b69b771528c13e55f8a8e58d1d9ef359136cc193bfcc92ef6e784b0b0445ef4aded2e360a0b7fe83cc06628299da4f74'
