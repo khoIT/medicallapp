bf69bb26575738aa54f5c1a048963a915b56dd21
Khoi Tran <emails.khoi@gmail.com>
2014-07-31 20:17:08 -0700

    Fixing survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,7 +1,6 @@
 class Survey < ActiveRecord::Base
   attr_accessible :id, :edu_final_1,
                        :edu_final_2,
-                       :edu_final_3,
-
-  has_many: medicine
+                       :edu_final_3
+  has_many :medicine
 end
