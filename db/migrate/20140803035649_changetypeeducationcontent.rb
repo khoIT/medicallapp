256680d016eeb371eedc254a39ac5275b9b6733d
Khoi Tran <emails.khoi@gmail.com>
2014-08-03 03:56:06 -0700

    fix bug migration syntax

--- db/migrate/20140803035649_changetypeeducationcontent.rb
+++ db/migrate/20140803035649_changetypeeducationcontent.rb
@@ -1,10 +1,10 @@
 class Changetypeeducationcontent < ActiveRecord::Migration
   def up
-    change_column :education, :content, :text
+    change_column :educations, :content, :text
   end
 
   def down
-    change_column :education, :content, :string
+    change_column :educations, :content, :string
   end
 end
 
